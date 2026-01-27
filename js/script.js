a70b42e36a0162b0b09c86f42c12acffa1637a22
Krystian Błasiak <blasiakkrystian@o2.pl>
2026-01-27 19:01:48 +0100

    Add code from exercise submodule 4.3

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, playerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Zagrałem ' + 'papier');
