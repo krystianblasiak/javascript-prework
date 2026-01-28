c8bce44500f5f15b0d4037e62b5d7435b8a43046
Krystian Błasiak <blasiakkrystian@o2.pl>
2026-01-28 15:50:59 +0100

    Add function if to script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 var computerMove, playerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var randomNumber = Math.floor(Math.random() * 3 + 1)
+
+if(randomNumber == 1) {
+    computerMove = "kamień"
+}
+else if(randomNumber == 2) {
+    computerMove = "papier"
+}
+else if(randomNumber == 3){
+    computerMove = "nożyce"
+}
+else {
+    computerMove = 'nieznany ruch';
+}
+
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 playerMove = 'papier';
 printMessage('Zagrałem ' + 'papier');
